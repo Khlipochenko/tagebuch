c52087b237ab4eced91d0995a1a906706fe56752
Natalia <khlipochenko@gmail.com>
2025-01-17 13:04:15 +0100

    connect DB

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import {connect }from './utils/connect.js'
-//import { userRouter } from './routes/userRouter.js'
+import { userRouter } from './routes/userRouter.js'
 dotenv.config()
 const app= express()
 app.use(express.json())
 app.use(cors())
-//app.use('/users', userRouter)
+app.use('/users', userRouter)
 app.get('/',(req,res)=>res.send('API Working'))
   app.get('/test', (req, res) => {
     res.status(200).json({ message: 'Hello from test!' });
@@ -19,7 +19,6 @@ app.use((err, req,res,next)=>{
 })
 const PORT=process.env.PORT ||5000
 
-//connect().then(()=>{
+connect().then(()=>{
     app.listen (PORT, ()=> console.log(`Server läuft auf http://localhost:${PORT}`))
-//})
-//export default app;
+})
