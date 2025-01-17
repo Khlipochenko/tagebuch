162467109e3a0843a03262c9f13b8cf875eb3fdd
Natalia <khlipochenko@gmail.com>
2025-01-17 10:44:01 +0100

    change

--- server/server.js
+++ server/server.js
@@ -2,15 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { connect } from './utils/connect.js'
-import { userRouter } from './routes/userRouter.js'
+//import { userRouter } from './routes/userRouter.js'
 dotenv.config()
 const app= express()
 app.use(express.json())
 app.use(cors())
 //app.use('/users', userRouter)
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello from Vercel!' });
-  });
+app.get('/',(req,res)=>res.send('API Working'))
   app.get('/test', (req, res) => {
     res.status(200).json({ message: 'Hello from test!' });
   });
@@ -24,4 +22,4 @@ const PORT=process.env.PORT ||5000
 connect().then(()=>{
     app.listen (PORT, ()=> console.log(`Server läuft auf http://localhost:${PORT}`))
 })
-export default app;
+//export default app;
