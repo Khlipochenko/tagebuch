26ffb65d071ec72913ab1d3d4215657d57bb429e
Natalia <khlipochenko@gmail.com>
2025-01-17 22:40:32 +0100

    change db

--- server/utils/connect.js
+++ server/utils/connect.js
@@ -4,7 +4,7 @@ dotenv.config()
 export const connect= async()=>{
     const MONGODB_URL=process.env.MONGODB_URL
     if(MONGODB_URL){
-        await mongoose.connect(MONGODB_URL)
+        await mongoose.connect(`${MONGODB_URL}tagebuch`)
         console.log('MD connected');
     }else{
         throw new Error('Error: MONGODB_URL not found');
