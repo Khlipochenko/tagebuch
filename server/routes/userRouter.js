cad83d667205a4cac76dd34477e90148f0ca01b7
Natalia <khlipochenko@gmail.com>
2025-01-23 13:55:02 +0100

    change

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,15 +1,22 @@
 import { Router } from "express";
 import { Notiz } from "../models/Notiz.js";
+import { User } from "../models/Users.js";
 export const userRouter=Router()
 userRouter.post('/write',async(req,res,next)=>{
     const userId=req.auth.userId
+    
     const  {title, text, datum, images}=req.body
     try{
+        const user= User.findById(userId)
+        if(user){
     await Notiz.create({
-    title, text,datum,userId
+    title, text,datum,userId:user._id
    }
 
-)
+)}
+else{
+    res.status(404).json({success:false, message:'User not found'})
+}
 res.sendStatus(201)
 }catch(error){
     console.log(error);
