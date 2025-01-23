d43d118b09cfd00366890e6a9cba13902b879ced
Natalia <khlipochenko@gmail.com>
2025-01-23 09:35:18 +0100

    change

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 export const userRouter=Router()
-userRouter.post('write',async(req,res,next)=>{
+userRouter.post('/write',async(req,res,next)=>{
     const userId=req.auth.userId
     console.log(userId);
     res.json(userId)
