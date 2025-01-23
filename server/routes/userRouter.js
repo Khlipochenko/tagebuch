f2a5e674a8dab004c33a3d10be5ae92dae501bcb
Natalia <khlipochenko@gmail.com>
2025-01-23 11:38:47 +0100

    change

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import { Notiz } from "../models/Notiz.js";
 export const userRouter=Router()
 userRouter.post('/write',async(req,res,next)=>{
     const userId=req.auth.userId
-    console.log(req.auth);
-    console.log(userId);
-    res.json(String(userId))
+    const  {title, text, datum, images}=req.body
+    try{
+    await Notiz.create({
+    title, text,datum,images,userId
+   })}catch(e){
+    console.log(error);
+    rex.status(400).json(error)
+   }
 
 })
