c09cd8c4e0f9e2a8c20f15d48112acbe066229d5
Natalia <khlipochenko@gmail.com>
2025-01-23 11:56:54 +0100

    clerk express add

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ import cors from 'cors'
 import {connect }from './utils/connect.js'
 import { userRouter } from './routes/userRouter.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import { clerkMiddleware } from '@clerk/express'
 dotenv.config()
 const app= express()
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 app.use('/users', userRouter)
 app.get('/',(req,res)=>res.send('API Working'))
 app.post('/webhooks', clerkWebhooks)
