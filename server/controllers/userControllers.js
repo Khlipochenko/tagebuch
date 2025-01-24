3219621027d5ff4e0fbd2c3e7ed94a6b6f84f4b2
Natalia <khlipochenko@gmail.com>
2025-01-24 13:12:10 +0100

    save

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -36,7 +36,7 @@ export const notizSchreiben=async(req,res,next)=>{
 else{
     res.status(404).json({success:false, message:'User not found'})
 }
-res.status(201).json({success:true, message:"Neue Notiz hinzugefügt"})
+res.status(201).json({success:true, message:`Neue Notiz hinzugefügt ${user._id}`})
 }catch(error){
     console.log(error);
     res.status(400).json(error)
