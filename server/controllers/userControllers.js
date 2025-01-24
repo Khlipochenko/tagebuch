54ba465ab9be9ac4d744ccc7388a9a794a1abbd2
Natalia <khlipochenko@gmail.com>
2025-01-24 09:30:32 +0100

    change

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import { User } from "../models/Users";
+
+//Notiz speichern
+export const notizSchreiben=async(req,res,next)=>{
+    const userId=req.auth.userId
+    const images=req.files
+    
+    const  {title, text, datum, }=req.body
+    console.log('req', req);
+  
+    try{
+        const user= User.findById(userId)
+        if(user){
+     const imagesUpload=await cloudinary.uploader.upload(images.path)      
+    await Notiz.create({
+    title:title, 
+    text:text,
+    datum:datum,
+    userId:user._id,
+    images:imagesUpload.secure_url
+   }
+
+)}
+else{
+    res.status(404).json({success:false, message:'User not found'})
+}
+res.sendStatus(201)
+}catch(error){
+    console.log(error);
+    res.status(400).json(error)
+   }
+
+}
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,31 +1,7 @@
 import { Router } from "express";
-import { Notiz } from "../models/Notiz.js";
-import { User } from "../models/Users.js";
+
+import { upload } from "../utils/multer.js";
+import { notizSchreiben } from "../controllers/userControllers.js";
+
 export const userRouter=Router()
-userRouter.post('/write',async(req,res,next)=>{
-    const userId=req.auth.userId
-    
-    const  {title, text, datum, images}=req.body
-    console.log('req', req);
-  
-    try{
-        const user= User.findById(userId)
-        if(user){
-    await Notiz.create({
-    title:title, 
-    text:text,
-    datum:String(datum),
-    userId:user._id
-   }
-
-)}
-else{
-    res.status(404).json({success:false, message:'User not found'})
-}
-res.sendStatus(201)
-}catch(error){
-    console.log(error);
-    res.status(400).json(error)
-   }
-
-})
+userRouter.post('/write',upload.array('images'), notizSchreiben)
