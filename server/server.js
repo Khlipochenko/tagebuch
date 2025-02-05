13882a2bd1f7ddb547badcd43c079743abe5fd08
Natalia Panova <khlipochenko@gmail.com>
2025-02-05 09:51:50 +0100

    add origin direkt

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use(cors( {
     credentials: true,
-    origin: [process.env.URL],
+    origin: ['https://tagebuch-server.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }))
