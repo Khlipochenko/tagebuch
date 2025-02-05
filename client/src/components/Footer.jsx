835e132b39646410299184fad42ba24739d83489
Natalia Panova <khlipochenko@gmail.com>
2025-02-05 21:04:42 +0100

    style

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer=()=>{
     return(
         <div className="fixed bottom-0 w-screen h-auto  bg-teal-800 py-2  text-white px-4  flex justify-between items-center">
         <p className="max-sm:text-sm">
-        © 2025 Panova Natalia – Alle Rechte vorbehalten.</p>
+        © 2025 Panova Natalia <span className="max-sm:hidden"> – Alle Rechte vorbehalten.</span></p>
         <a className="px-4 text-xl  sm:hover:text-red-500 sm:hover:scale-110" href="https://github.com/Khlipochenko" target="_blank"><FaGithub /></a>
         </div>
     )
