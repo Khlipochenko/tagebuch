f468cbe2f4f57f417d3a53e468fb05b4611601e7
Natalia Panova <khlipochenko@gmail.com>
2025-02-05 09:44:51 +0100

    add url in server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use(cors( {
     credentials: true,
-    origin: ['http://localhost:5173'],
+    origin: [process.env.URL],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }))
