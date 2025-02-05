ccf2777f8cb62e841243364a4dc450f395f2130b
Natalia Panova <khlipochenko@gmail.com>
2025-02-05 13:23:47 +0100

    styling w-full write

--- client/src/Pages/Write.jsx
+++ client/src/Pages/Write.jsx
@@ -139,7 +139,7 @@ function handleOnClickDeleteImage(index) {
       
 
     <div className=" min-h-screen my-20 ">
-    <form onSubmit={(e)=>handleOnSubmit(e)} className="flex flex-col items-center justify-center w-2/4 mx-auto mt-12">
+    <form onSubmit={(e)=>handleOnSubmit(e)} className="flex flex-col items-center justify-center w-2/4 mx-auto mt-12 max-sm:w-full">
       <label className="self-start" for="date">
         Datum:
         <input
