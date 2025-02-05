c9417da32aa4e395552db25009485cf2392d80b6
Natalia Panova <khlipochenko@gmail.com>
2025-02-05 09:59:10 +0100

    change

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use(cors( {
     credentials: true,
-    origin: [process.env.URL],
+    origin: ['https://tagebuch-eta.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }))
