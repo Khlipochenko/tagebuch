b93849a35ad432684378ad8f60710e35b3ad1bac
Natalia Panova <khlipochenko@gmail.com>
2025-02-05 13:13:48 +0100

    styling

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -69,8 +69,8 @@ useEffect(() => {
 }
       </div>):(
         <div className=" flex-grow bg-[url('./assets/bild.jpeg')] bg-cover max-sm:bg-center">
-        <div className='flex justify-center flex-col items-center max-sm:items-start '>
-           <h1 className=' text-3xl mx-auto w-3/5  mt-96 text-center px-10 tracking-wide leading-loose font-medium text-emerald-950 h-auto  max-sm:text-lg max-sm:mt-20'>Bitte einloggen oder registrieren, um Zugang zu erhalten und dein Tagebuch zu starten.</h1>
+        <div className='flex justify-center flex-col items-center  '>
+           <h1 className=' text-3xl mx-auto w-3/5  mt-96 text-center px-10 tracking-wide leading-loose font-medium text-emerald-950 h-auto  max-sm:text-lg  max-sm:mt-24'>Bitte einloggen oder registrieren, um Zugang zu erhalten und dein Tagebuch zu starten.</h1>
            <button className='drop-shadow-lg px-4  py-1 rounded bg-teal-500  text-white sm:hover:bg-teal-900' onClick={()=>setLoginModus(true)}>Login</button>
            </div>  
         </div>
