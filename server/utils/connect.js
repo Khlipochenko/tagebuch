48031f2ed2a6a3b6d8ba3b27849349eae63aa1da
Natalia Panova <143701784+Khlipochenko@users.noreply.github.com>
2026-04-19 23:54:09 +0200

    Add logging for MongoDB connection URL
    Log the MongoDB URL from environment variables.

--- server/utils/connect.js
+++ server/utils/connect.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 export const connect= async()=>{
     const MONGODB_URL=process.env.MONGODB_URL
+    console.log("ENV:", process.env.MONGODB_URL);
     if(MONGODB_URL){
         await mongoose.connect(`${MONGODB_URL}tagebuch`)
         console.log('MD connected');
